ffbee1ba859745cc16bbcba5055525c6f098affb
Vlad <asdzorg@yandex.ru>
2020-04-19 23:30:45 +0300

    combined json from users and messages

---
+++ redme.md
@@ -0,0 +1,11 @@
+
+## About
+
+Real-time listener database MySQL on HTML for [Blog](https://github.com/VladDvl/blog).
+It allows you to create realtime chat in a laravel application using a separate node.js server.
+
+## Using
+
+* Create script that will receive JSON and insert html elements to page.
+* Connect the script to the site.
+* Run: node server.js.
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(3003)
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'shop',
+    database: 'blog',
     password: ''
 })
 
@@ -14,16 +14,17 @@ db.connect(function (err) {
     if (err) console.log(err)
 });
 // Define/initialize our global vars
-var notes = []
-var isInitNotes = false
-var socketCount = 0
+var notes = [];
+var isInitNotes = false;
+var socketCount = 0;
 global.new_id = 0;
 global.new_id_new = 0;
 global.arr = [];
+global.arr2 = [];
 io.sockets.on('connection', function (socket) {
     socket.emit('echo', 'server send message');
     setInterval(function () {
-        db.query('SELECT ID FROM device_log ORDER BY ID DESC LIMIT 1', function (err, row) {
+        db.query('SELECT ID FROM messages WHERE status="PUBLISHED" AND resiver_id IS NULL AND group_id IS NULL ORDER BY ID DESC LIMIT 1', function (err, row) {
             if (err) throw err;
             socket.emit('echo', global.arr);
             global.new_id = row[0].ID;
@@ -31,15 +32,32 @@ io.sockets.on('connection', function (socket) {
             if (new_id_new != new_id) {
                 socket.emit('echo', row[0].ID + ' --- --- \n');
                 global.new_id_new = global.new_id;
-                db.query('SELECT * FROM device_log ORDER BY ID DESC LIMIT 100', function (err, rows) {
+                db.query('SELECT * FROM messages WHERE status="PUBLISHED" AND resiver_id IS NULL AND group_id IS NULL ORDER BY ID ASC LIMIT 100', function (err, rows) {
                     if (err) throw err;
                     console.log('Data received from Db:\n');
                     console.log(rows);
                     global.arr = rows;
-                    socket.emit('echo', rows);
+                    //socket.emit('echo', rows);
                     //socket.emit('showrows', rows);
                 });
+				db.query('SELECT id, name, avatar FROM users', function (err, rows2) {
+					if (err) throw err;
+					console.log('Data received from Db:\n');
+					console.log(rows2);
+					global.arr2 = rows2;
+				});
             }
+            for( msg in global.arr )
+            {
+                for( usr in global.arr2 )
+                {
+                    if( global.arr[msg].sender_id == global.arr2[usr].id ) {
+                        global.arr[msg].name = global.arr2[usr].name;
+                        global.arr[msg].avatar = global.arr2[usr].avatar;
+                    }
+                }
+            }
+            //console.log('result data:', global.arr);
         });
     }, 2000);
     console.log('connected2');
